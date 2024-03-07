9754936c4a006e6ed86b650d2c896f31b8a47137
Wallat <wbilal18@student.aau.dk>
2024-03-07 14:40:27 +0100

    made changes moving only the omtp_corse folder.

--- workspace/build/omtp_course/omtp_lecture1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wallat/Desktop/Object-manipulation-and-task-planning-/workspace/src/omtp_course/omtp_lecture1
-# Build directory: /home/wallat/Desktop/Object-manipulation-and-task-planning-/workspace/build/omtp_course/omtp_lecture1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
