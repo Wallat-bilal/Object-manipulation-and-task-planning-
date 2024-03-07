890a68b82fcce3f0f971f6f4e7b761f51aa14d04
Wallat <wbilal18@student.aau.dk>
2024-03-07 14:43:48 +0100

    wrong changes back to normal

---
+++ workspace/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/wallat/Desktop/Object-manipulation-and-task-planning-/workspace/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/wallat/Desktop/Object-manipulation-and-task-planning-/workspace/devel;/opt/ros/noetic'.split(';') if '/home/wallat/Desktop/Object-manipulation-and-task-planning-/workspace/devel;/opt/ros/noetic' != '' else []
